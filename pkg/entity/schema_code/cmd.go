d9cf7b7819759ae215b7e2f95c0a6f532510bd53
Bart van Deenen <bartvandeenen@streammachine.io>
2021-08-02 12:23:51 +0200

    feat(schema-code): getting schema-code works

--- pkg/entity/schema_code/cmd.go
+++ pkg/entity/schema_code/cmd.go
@@ -6,13 +6,24 @@ import (
 )
 
 func GetCmd() *cobra.Command {
-	return &cobra.Command{
+	getCommand := &cobra.Command{
 		Use:   "schema-code (schema-ref)",
 		Short: "Get schema code archive by schema-ref",
 		Run: func(cmd *cobra.Command, args []string) {
-			get(&args[0])
+			get(cmd, &args[0])
 		},
-		Args: cobra.ExactArgs(1), // the schema ref
+		Args:              cobra.ExactArgs(1), // the schema ref
 		ValidArgsFunction: schema.NamesCompletion,
 	}
+	flags := getCommand.Flags()
+	flags.String(languageFlag, "", "which programming language")
+	flags.String(filenameFlag, "", "tar file name")
+	flags.Bool(overwriteFlag, false, "do we allow overwriting an existing file")
+	_ = getCommand.MarkFlagRequired(languageFlag)
+	_ = getCommand.RegisterFlagCompletionFunc(languageFlag, languageCompletion)
+	return getCommand
+}
+
+func languageCompletion(cmd *cobra.Command, args []string, complete string) ([]string, cobra.ShellCompDirective) {
+	return []string{"java","typescript","python"}, cobra.ShellCompDirectiveDefault
 }
--- pkg/entity/schema_code/schema_code.go
+++ pkg/entity/schema_code/schema_code.go
@@ -2,15 +2,21 @@ package schema_code
 
 import (
 	"context"
+	"github.com/spf13/cobra"
 	"github.com/streammachineio/api-definitions-go/api/schemas/v1"
 	"google.golang.org/grpc"
+	"os"
 	"streammachine.io/strm/pkg/common"
 	"streammachine.io/strm/pkg/entity/schema"
 	"streammachine.io/strm/pkg/util"
 )
 
 // strings used in the cli
-const ()
+const (
+	languageFlag="language"
+	filenameFlag="output-file"
+	overwriteFlag="overwrite"
+)
 
 var client schemas.SchemasServiceClient
 var apiContext context.Context
@@ -21,17 +27,37 @@ func SetupClient(clientConnection *grpc.ClientConn, ctx context.Context) {
 	client = schemas.NewSchemasServiceClient(clientConnection)
 }
 
-func get(name *string) {
-	schemaCode := GetSchemaCode(name)
-	util.Print(schemaCode)
+func get(cmd *cobra.Command, schemaRef *string) {
+	outputFile := GetSchemaCode(cmd, schemaRef)
+	println("Saved to", outputFile)
+
 }
 
-func GetSchemaCode(name *string) *schemas.GetSchemaCodeResponse {
+func GetSchemaCode(cmd *cobra.Command, name *string) string {
+	flags := cmd.Flags()
+	language:= util.GetStringAndErr(flags, languageFlag)
+	outputFile := util.GetStringAndErr(flags, filenameFlag)
+	overwrite := util.GetBoolAndErr(flags, overwriteFlag)
 	req := &schemas.GetSchemaCodeRequest{
 		BillingId: common.BillingId,
-		Language: "typescript",
+		Language: language,
 		Ref: schema.Ref(name)}
 	schemaCode, err := client.GetSchemaCode(apiContext, req)
 	common.CliExit(err)
-	return schemaCode
+	if len(outputFile)==0 {
+		outputFile = schemaCode.Filename
+	}
+	if !overwrite {
+		_, err = os.Stat(outputFile)
+		if !os.IsNotExist(err) {
+			common.CliExit("Not overwriting "+ outputFile)
+		}
+	}
+	saveFile(schemaCode, outputFile)
+	return outputFile
+}
+
+func saveFile(code *schemas.GetSchemaCodeResponse, file string) {
+	err := os.WriteFile(file, code.Data, 0666 )
+	common.CliExit(err)
 }
