9e2247a4ce96cac6d7a3f8726a8978811f173c55
Robin Trietsch <robin@streammachine.io>
2022-04-25 16:27:40 +0200

    fix(strm-1003): exit upon errors with simulating events

--- pkg/simulator/sims.go
+++ pkg/simulator/sims.go
@@ -2,6 +2,7 @@ package sim
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -35,16 +36,21 @@ func (s ModernSender) Send(event StrmPrivacyEvent, token string) {
 	b := &bytes.Buffer{}
 	err := event.Serialize(b)
 	common.CliExit(err)
+
 	req, err := http.NewRequest("POST", s.Gateway, b)
 	common.CliExit(err)
+
 	req.Header.Set("Strm-Schema-Ref", s.Schema)
 	req.Header.Set("Authorization", "Bearer "+token)
+
 	resp, err := s.Client.Do(req)
-	if err != nil || resp.StatusCode != 204 {
-		if resp != nil {
-			defer resp.Body.Close()
-			body, err := io.ReadAll(resp.Body)
-			fmt.Printf("%v %s\n", err, string(body))
-		}
+
+	if err != nil {
+		common.CliExit(errors.New(fmt.Sprintf("An error occurred while simulating events.\nError: %v", err)))
+	} else if resp.StatusCode != 204 {
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+
+		common.CliExit(errors.New(fmt.Sprintf("The simulated event sent resulted in an error (expected status is not 204).\nError: %v, Response Body: %s", err, string(body))))
 	}
 }
