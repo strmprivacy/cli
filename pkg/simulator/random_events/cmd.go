ab822c85cdf8a71a3a308931093cae2b3fc8c449
Robin Trietsch <robin@strmprivacy.io>
2022-07-06 14:58:12 +0200

    feat(strm-1258): use client_credentials grant for simulator and websocket

--- pkg/simulator/random_events/cmd.go
+++ pkg/simulator/random_events/cmd.go
@@ -2,9 +2,7 @@ package random_events
 
 import (
 	"github.com/spf13/cobra"
-	"strmprivacy/strm/pkg/common"
 	"strmprivacy/strm/pkg/entity/stream"
-	"strmprivacy/strm/pkg/simulator"
 )
 
 var longDoc = `The global ` + "`simulate`" + ` command runs a simulation with random events against a given Source Stream, using the ClickStream
@@ -18,18 +16,18 @@ func RunCmd() (cmd *cobra.Command) {
 		Short:             "Run a simulator that will send random events to a stream",
 		Long:              longDoc,
 		DisableAutoGenTag: true,
-		Run:               func(cmd *cobra.Command, args []string) { run(cmd, &args[0]) },
+		Run:               func(cmd *cobra.Command, args []string) { randomEvents(cmd, &args[0]) },
 		Args:              cobra.ExactArgs(1), // the stream name
 		ValidArgsFunction: stream.SourceNamesCompletion,
 	}
+
 	flags := simCmd.Flags()
-	flags.String(sim.EventsApiUrlFlag, "https://events.strmprivacy.io/event", "Endpoint to send events to")
-	flags.Int(sim.IntervalFlag, 1000, "Interval in ms. between simulated events")
-	flags.Int(sim.SessionRangeFlag, 1000, "Number of different sessions being generated")
-	flags.String(sim.SessionPrefixFlag, "session", "Prefix string for sessions")
-	flags.String(common.ClientIdFlag, "", "Client id to be used for sending data")
-	flags.String(common.ClientSecretFlag, "", "Client secret to be used for sending data")
-	flags.Bool(sim.QuietFlag, false, "don't spam stderr")
-	flags.StringSlice(sim.ConsentLevelsFlag, []string{"", "0", "0/1", "0/1/2", "0/1/2/3"}, "consent levels to be simulated")
+	flags.String(EventsApiUrlFlag, "https://events.strmprivacy.io/event", "Endpoint to send events to")
+	flags.Int(IntervalFlag, 1000, "Interval in ms. between simulated events")
+	flags.Int(SessionRangeFlag, 1000, "Number of different sessions being generated")
+	flags.String(SessionPrefixFlag, "session", "Prefix string for sessions")
+	flags.Bool(QuietFlag, false, "Do not print to stderr")
+	flags.StringSlice(ConsentLevelsFlag, []string{"", "0", "0/1", "0/1/2", "0/1/2/3"}, "consent levels to be simulated")
+
 	return simCmd
 }
---
+++ pkg/simulator/random_events/random_events.go
@@ -0,0 +1,115 @@
+package random_events
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"golang.org/x/oauth2/clientcredentials"
+	"math/rand"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/strmprivacy/api-definitions-go/v2/api/entities/v1"
+	"strmprivacy/strm/pkg/common"
+	"strmprivacy/strm/pkg/entity/stream"
+	"strmprivacy/strm/pkg/util"
+)
+
+const (
+	IntervalFlag      = "interval"
+	EventsApiUrlFlag  = "events-api-url"
+	SessionRangeFlag  = "session-range"
+	SessionPrefixFlag = "session-prefix"
+	ConsentLevelsFlag = "consent-levels"
+	QuietFlag         = "quiet"
+	SchemaFlag        = "schema"
+)
+
+// start a random simulator
+func randomEvents(cmd *cobra.Command, streamName *string) {
+	s := getStream(streamName)
+
+	flags := cmd.Flags()
+	interval := time.Duration(util.GetIntAndErr(flags, IntervalFlag))
+	sessionRange := util.GetIntAndErr(flags, SessionRangeFlag)
+	sessionPrefix := util.GetStringAndErr(flags, SessionPrefixFlag)
+	gateway := util.GetStringAndErr(flags, EventsApiUrlFlag)
+	quiet := util.GetBoolAndErr(flags, QuietFlag)
+	consentLevels, err := flags.GetStringSlice(ConsentLevelsFlag)
+	common.CliExit(err)
+
+	schema := util.GetStringAndErr(flags, SchemaFlag)
+
+	if EventGenerators[schema] == nil {
+		common.CliExit(errors.New(fmt.Sprintf("The schema %s is not supported in the CLI", schema)))
+	}
+
+	if len(consentLevels) == 0 {
+		common.CliExit(errors.New(fmt.Sprintf("%v is not a valid set of consent levels", consentLevels)))
+	}
+
+	if !quiet {
+		fmt.Printf("Starting to simulate random %s events to stream %s. ",
+			schema, *streamName)
+		fmt.Printf("Sending one event every %d ms.\n", interval)
+	}
+
+	simulate(s, gateway, schema, sessionPrefix, sessionRange, consentLevels, interval, quiet)
+}
+
+func simulate(s *entities.Stream, gateway string, schema string, sessionPrefix string, sessionRange int, consentLevels []string, interval time.Duration, quiet bool) {
+	client := authenticatedHttpClient(s.Credentials[0])
+	f := EventGenerators[schema]
+
+	simulator := Simulator{Client: client, Gateway: gateway, Schema: schema}
+
+	var ct = 0
+	now := time.Now()
+	for {
+		sessionId := fmt.Sprintf("%s-%d", sessionPrefix, rand.Intn(sessionRange))
+		event := f(randomConsentLevels(consentLevels), sessionId)
+
+		go simulator.Send(event)
+		ct += 1
+		time.Sleep(interval * time.Millisecond)
+		if !quiet && time.Now().Sub(now) > 5*time.Second {
+			println("Sent", ct, "events")
+			now = time.Now()
+		}
+	}
+}
+
+// randomConsentLevels returns a slice of integers for the simulated event.
+// It starts with [ "", "0", "0/1", "3/8", "3/7/10", ...] so a slice of strings that define
+// what we want to send. This method picks a random one.
+func randomConsentLevels(levels []string) []int32 {
+	l := strings.Split(levels[rand.Intn(len(levels))], "/")
+	if len(l) == 1 && l[0] == "" {
+		// No consent at all
+		return []int32{}
+	}
+	return util.StringsArrayToInt32(l)
+}
+
+func getStream(streamName *string) *entities.Stream {
+	s := stream.Get(streamName, false).StreamTree.Stream
+
+	if len(s.LinkedStream) != 0 {
+		common.CliExit(errors.New("simulators cannot be randomEvents on a derived s"))
+	}
+
+	return s
+}
+
+func authenticatedHttpClient(credentials *entities.Credentials) http.Client {
+	config := clientcredentials.Config{
+		ClientID:     credentials.ClientId,
+		ClientSecret: credentials.ClientSecret,
+		TokenURL:     fmt.Sprintf("%v/auth/realms/streams/protocol/openid-connect/token", common.ApiAuthHost),
+		Scopes:       []string{"offline_access"},
+	}
+
+	return *config.Client(context.Background())
+}
