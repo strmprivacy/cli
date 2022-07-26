9b71b214b8dab04fa325404c2ec2deecbc921339
Ivan Plantevin <ivan@strmprivacy.io>
2022-07-26 09:43:17 +0200

    feat(strm-1433): implement invite users command (#98)
    * feat(strm-1433): implement invite users command
    
    * feat(strm-1433): handle issues from response
    
    * fix: update api defs version
    
    Co-authored-by: Bob van den Hoogen <bob@strmprivacy.io>

---
+++ pkg/entity/organization/cmd.go
@@ -0,0 +1,33 @@
+package organization
+
+import (
+	"github.com/spf13/cobra"
+)
+
+const (
+	userEmailsFileFlag = "user-emails-file"
+)
+
+var inviteLongDoc = `Invite one or more users to your organization, by email.
+
+Either provide the emails comma-separated on the command line, or pass a file
+with the -f flag containing one email address per line.
+
+### Usage`
+
+func InviteUsersCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:               "users [first-email,second-email,...]",
+		Short:             "Invite users to your organization by email",
+		Long:              inviteLongDoc,
+		DisableAutoGenTag: true,
+		Run: func(cmd *cobra.Command, args []string) {
+			inviteUsers(args, cmd)
+		},
+		Args: cobra.MaximumNArgs(1),
+	}
+
+	flags := cmd.Flags()
+	flags.StringP(userEmailsFileFlag, "f", "", "file with users to invite, one email per line")
+	return cmd
+}
---
+++ pkg/entity/organization/organization.go
@@ -0,0 +1,71 @@
+package organization
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/strmprivacy/api-definitions-go/v2/api/organizations/v1"
+	"google.golang.org/grpc"
+	"io/ioutil"
+	"strings"
+	"strmprivacy/strm/pkg/common"
+	"strmprivacy/strm/pkg/util"
+)
+
+var client organizations.OrganizationsServiceClient
+var apiContext context.Context
+
+func SetupClient(clientConnection *grpc.ClientConn, ctx context.Context) {
+	apiContext = ctx
+	client = organizations.NewOrganizationsServiceClient(clientConnection)
+}
+
+func inviteUsers(args []string, cmd *cobra.Command) {
+	if apiContext == nil {
+		common.CliExit(errors.New(fmt.Sprint("No login information found. Use: `dstrm auth login` first.")))
+	}
+	emails := getEmails(args, cmd)
+	var invites []*organizations.UserInvite
+	for _, email := range emails {
+		invites = append(invites, &organizations.UserInvite{Email: email})
+	}
+	req := &organizations.InviteUsersRequest{
+		UserInvites: invites,
+	}
+	response, err := client.InviteUsers(apiContext, req)
+	common.CliExit(err)
+	handleInviteResponse(response)
+}
+
+func handleInviteResponse(response *organizations.InviteUsersResponse) {
+	fmt.Println(fmt.Sprintf("Invited %d users to your organization.", response.InviteCount))
+	if len(response.Issues) > 0 {
+		fmt.Println(fmt.Sprintf("There were %d invites with issues:\n", len(response.Issues)))
+		inviteIssuesPrinter{}.Print(response.Issues)
+	}
+}
+
+func getEmails(args []string, cmd *cobra.Command) []string {
+	emailsFile := util.GetStringAndErr(cmd.Flags(), userEmailsFileFlag)
+	var emails []string
+
+	if len(args) == 0 && emailsFile == "" {
+		common.CliExit(errors.New(fmt.Sprint("Either provide comma-separated emails on the command line, or a file containing emails.")))
+	} else if len(args) > 0 {
+		emails = strings.Split(args[0], ",")
+	} else {
+		emails = read(emailsFile)
+	}
+	return emails
+}
+
+func read(emailsFile string) []string {
+	buf, err := ioutil.ReadFile(emailsFile)
+	common.CliExit(err)
+	splitFn := func(c rune) bool {
+		return c == '\n'
+	}
+	// FieldsFunc is used instead of split to filter out any (trailing) empty lines
+	return strings.FieldsFunc(string(buf), splitFn)
+}
