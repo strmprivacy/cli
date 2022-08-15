0697657585bad1e85d2dcec94ecf40199d56b910
Bart van Deenen <bartvandeenen@streammachine.io>
2022-08-15 09:14:36 +0200

    fix(fix/doc-scripts): forgot to build docs first

---
+++ scripts/generate_docs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    SED="gsed"
+else
+    SED="sed"
+fi
+
+if [[ $APIS_EMAIL != "" ]]
+then
+  git config --global user.email "${APIS_EMAIL}"
+  git config --global user.name "${APIS_USERNAME}"
+  tag_name="${GITHUB_REF##*/}"
+else
+  tag_name="local_test"
+fi
+
+rm -rf generated_docs docs
+mkdir generated_docs
+
+if [[ $APIS_EMAIL == "" ]]
+then
+  make
+  ./dist/strm --generate-docs > /dev/null 2>&1
+else
+  ./dist/strm_linux_amd64_v1/strm --generate-docs > /dev/null 2>&1
+fi
+./scripts/generate_docs.py
