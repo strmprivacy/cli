e13606ecfd84f8b99cb609d1dac0b3fe8f2a9c10
Robin Trietsch <robin@strmprivacy.io>
2022-09-29 16:57:40 +0200

    fix: publish docs

--- scripts/publish_docs.sh
+++ scripts/publish_docs.sh
@@ -3,12 +3,15 @@
 
 if [[ $APIS_EMAIL != "" ]]
 then
+  echo "Setting git config"
   git config --global user.email "${APIS_EMAIL}"
   git config --global user.name "${APIS_USERNAME}"
   tag_name="${GITHUB_REF##*/}"
 else
   tag_name="local"
 fi
+
+cd docs
 
 if [[ $GITHUB_TOKEN == "" ]]
 then
